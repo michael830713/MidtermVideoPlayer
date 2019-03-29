f57cacc799db4c27e566f65bb90d6e733554b37c
mike <michael830713@gmail.com>
2019-03-29 10:00:48 +0800

    added the default video view and controller

--- app/src/main/java/com/example/midtermvideoplayer/MainActivity.java
+++ app/src/main/java/com/example/midtermvideoplayer/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.midtermvideoplayer;
 
+import android.graphics.PixelFormat;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity  {
+    private VideoView video;
+    private MediaController ctlr;
+    private static final String VIDEO_PATH = "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/taeyeon.mp4";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        getWindow().setFormat(PixelFormat.TRANSLUCENT);
         setContentView(R.layout.activity_main);
+
+        File clip=new File(Environment.getExternalStorageDirectory(),
+                "test.mp4");
+
+
+            video=(VideoView)findViewById(R.id.videoView);
+            video.setVideoPath(VIDEO_PATH);
+
+            ctlr=new MediaController(this);
+            ctlr.setMediaPlayer(video);
+            video.setMediaController(ctlr);
+            video.requestFocus();
+            video.start();
+        }
     }
-}
+
