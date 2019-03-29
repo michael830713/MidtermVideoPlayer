1d313ba02033c93db2ab1b6c47346d1c15d12cdc
mike <michael830713@gmail.com>
2019-03-29 11:52:31 +0800

    imported the sample project

--- app/src/main/java/com/example/midtermvideoplayer/MainActivity.java
+++ app/src/main/java/com/example/midtermvideoplayer/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.midtermvideoplayer;
 
+import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.MediaController;
@@ -15,28 +17,13 @@ import java.io.File;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity  {
-    private VideoView video;
-    private MediaController ctlr;
-    private static final String VIDEO_PATH = "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/taeyeon.mp4";
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        getWindow().setFormat(PixelFormat.TRANSLUCENT);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, VideoPlayerActivity.class));
+    }
 
-        File clip=new File(Environment.getExternalStorageDirectory(),
-                "test.mp4");
-
-
-            video=(VideoView)findViewById(R.id.videoView);
-            video.setVideoPath(VIDEO_PATH);
-
-            ctlr=new MediaController(this);
-            ctlr.setMediaPlayer(video);
-            video.setMediaController(ctlr);
-            video.requestFocus();
-            video.start();
-        }
     }
 
